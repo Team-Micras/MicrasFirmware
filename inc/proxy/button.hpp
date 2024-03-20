9282b370bfda9195325c447a656852199d193c27
PedroDeSanti <phmartinsanti@gmail.com>
2024-03-20 05:30:29 -0300

    ⚡️ Refactor button class to add status checking and edge detection

--- inc/proxy/button.hpp
+++ inc/proxy/button.hpp
@@ -10,37 +10,140 @@
 #define __BUTTON_HPP__
 
 #include "hal/gpio.hpp"
+#include <stdint.h>
 
 namespace proxy {
 /**
- * @brief Class for controlling Button
+ * @brief Class for controlling buttons.
  */
 class Button {
     public:
         /**
-         * @brief configuration structure for button
+         * @brief Enum for button status.
          */
-        struct Config {
-            hal::Gpio::Config gpio_config;
+        enum Status {
+            NO_PRESS,
+            SHORT_PRESS,
+            LONG_PRESS,
+            EXTRA_LONG_PRESS
         };
 
         /**
-         * @brief Constructor for the Button class
+         * @brief Enum for button pull resistor.
+         */
+        enum PullResistor {
+            PULL_UP,
+            PULL_DOWN,
+        };
+
+        /**
+         * @brief Configuration structure for button.
+         */
+        struct Config {
+            hal::Gpio::Config gpio_config;
+            PullResistor      pull_resistor;
+            uint16_t          debounce_delay_ms = 10;
+            uint16_t          long_press_delay_ms = 1000;
+            uint16_t          extra_long_press_delay_ms = 5000;
+        };
+
+        /**
+         * @brief Constructor for Button class.
          *
-         * @param button_config Configuration for the button
+         * @param button_config Button configuration.
          */
         Button(const Config& button_config);
 
         /**
-         * @brief Get the button state
+         * @brief Get button status.
          *
-         * @return true if the button is pressed, false otherwise
+         * @return Button status.
          */
-        bool get_state();
+        Status get_status();
+
+        /**
+         * @brief Check if button is pressed.
+         *
+         * @return True if button is pressed, false otherwise.
+         */
+        bool is_pressed();
+
+        /**
+         * @brief Check if button is released.
+         *
+         * @return True if button is released, false otherwise.
+         */
+        bool is_released();
+
+        /**
+         * @brief Check if button was just pressed.
+         *
+         * @return True if button was just pressed, false otherwise.
+         */
+        bool is_rising_edge() const;
+
+        /**
+         * @brief Check if button was just released.
+         *
+         * @return True if button was just released, false otherwise.
+         */
+        bool is_falling_edge() const;
 
     private:
-        hal::Gpio button_gpio;
-};
+        /**
+         * @brief Update button state.
+         */
+        void update_state();
+
+        /**
+         * @brief Get raw button reading.
+         *
+         * @return Button reading.
+         */
+        bool get_raw_reading() const;
+
+        /**
+         * @brief Button pressing delays.
+         */
+        const uint16_t debounce_delay_ms;
+        const uint16_t long_press_delay_ms;
+        const uint16_t extra_long_press_delay_ms;
+
+        /**
+         * @brief Gpio object for button.
+         */
+        hal::Gpio gpio;
+
+        /**
+         * @brief pull resistor configuration
+         */
+        PullResistor pull_resistor;
+
+        /**
+         * @brief timer to check if button is debouncing
+         */
+        uint32_t debounce_timer = 0;
+
+        /**
+         * @brief timer to determine type of button press
+         */
+        uint32_t status_timer = 0;
+
+        /**
+         * @brief flag to know when button is debouncing
+         */
+        bool is_debouncing = false;
+
+        /**
+         * @brief flag to know if button was being pressed
+         */
+        bool previous_state = false;
+
+        /**
+         * @brief flag to know if button is being pressed
+         */
+        bool current_state = false;
+};  // class Button
 }  // namespace proxy
 
 #endif // __BUTTON_HPP__
--- src/proxy/button.cpp
+++ src/proxy/button.cpp
@@ -4,16 +4,74 @@
  * @brief Proxy Button class source
  *
  * @date 03/2024
- *
  */
 
+#include "hal/mcu.hpp"
 #include "proxy/button.hpp"
 
 namespace proxy {
-Button::Button(const Config& button_config) : button_gpio(button_config.gpio_config) {
+Button::Button(const Config& button_config) :
+    debounce_delay_ms(button_config.debounce_delay_ms),
+    long_press_delay_ms(button_config.long_press_delay_ms),
+    extra_long_press_delay_ms(button_config.extra_long_press_delay_ms),
+    gpio(button_config.gpio_config),
+    pull_resistor(button_config.pull_resistor) {
 }
 
-bool Button::get_state() {
-    return this->button_gpio.read();
+void Button::update_state() {
+    bool raw_reading = this->get_raw_reading();
+
+    if ((raw_reading != this->current_state) && !this->is_debouncing) {
+        this->is_debouncing = true;
+        hal::mcu::reset_timer(this->debounce_timer);
+    } else if ((hal::mcu::get_timer_ms(this->debounce_timer) < debounce_delay_ms) && this->is_debouncing) {
+        if (this->current_state == raw_reading) {
+            this->is_debouncing = false;
+        }
+    } else {
+        this->is_debouncing = false;
+        this->previous_state = this->current_state;
+        this->current_state = raw_reading;
+    }
+}
+
+Button::Status Button::get_status() {
+    this->update_state();
+
+    if (this->is_rising_edge()) {
+        hal::mcu::reset_timer(this->status_timer);
+    } else if (this->is_falling_edge()) {
+        if (hal::mcu::get_timer_ms(this->status_timer) > extra_long_press_delay_ms) {
+            return EXTRA_LONG_PRESS;
+        } else if (hal::mcu::get_timer_ms(this->status_timer) > long_press_delay_ms) {
+            return LONG_PRESS;
+        } else {
+            return SHORT_PRESS;
+        }
+    }
+
+    return NO_PRESS;
+}
+
+bool Button::is_pressed() {
+    this->update_state();
+    return (this->current_state == true);
+}
+
+bool Button::is_released() {
+    this->update_state();
+    return (this->current_state == false);
+}
+
+bool Button::is_rising_edge() const {
+    return (this->current_state == true && this->previous_state == false);
+}
+
+bool Button::is_falling_edge() const {
+    return (this->current_state == false && this->previous_state == true);
+}
+
+bool Button::get_raw_reading() const {
+    return (this->gpio.read() == (bool) this->pull_resistor);
 }
 }  // namespace proxy
