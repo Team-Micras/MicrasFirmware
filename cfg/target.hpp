f5b4d339c626ad39b12f54c9d18387739da79a5b
PedroDeSanti <phmartinsanti@gmail.com>
2024-03-20 21:51:42 -0300

    🔧 Update proxy configurations in target.hpp
    Co-authored-by: Gabriel Cosme Barbosa <GabrielCosme@users.noreply.github.com>
    Co-authored-by: Matheus Rezende <Matheus3007@users.noreply.github.com>

--- cfg/target.hpp
+++ cfg/target.hpp
@@ -5,41 +5,66 @@
 
 #include "proxy/button.hpp"
 #include "proxy/led.hpp"
-#include "proxy/distance_sensor_array.hpp"
-#include "proxy/current_sensor_array.hpp"
+#include "proxy/distance_sensors.hpp"
+#include "proxy/current_sensors.hpp"
+#include "proxy/dip_switch.hpp"
 
 proxy::Led::Config led_config = {
-    .gpio_config = {
+    .gpio = {
         .port = GPIOA,
         .pin = GPIO_PIN_15
     }
 };
 
 proxy::Button::Config button_config = {
-    .gpio_config = {
+    .gpio = {
         .port = GPIOA,
         .pin = GPIO_PIN_12
     },
-    .pull_resistor = proxy::Button::PULL_UP
+    .pull_resistor = proxy::Button::PullResistor::PULL_UP
 };
 
-proxy::DistanceSensorArray<4>::Config distance_sensor_config = {
-    .sensors_adc_config = {
+proxy::DistanceSensors<4>::Config distance_sensor_config = {
+    .adc = {
         .handle = &hadc1,
         .init_function = MX_ADC1_Init
     },
-    .infrared_tim_config = {
-        .handle = &htim2,
-        .init_function = MX_TIM15_Init
-    },
-    .infrared_led_channel = TIM_CHANNEL_1
+    .infrared_pwm = {
+        .timer = {
+            .handle = &htim15,
+            .init_function = MX_TIM15_Init
+        },
+        .timer_channel = TIM_CHANNEL_1
+    }
 };
 
-proxy::CurrentSensorArray<2>::Config current_sensor_config = {
-    .adc_config = {
+proxy::CurrentSensors<2>::Config current_sensor_config = {
+    .adc = {
         .handle = &hadc2,
         .init_function = MX_ADC2_Init
-    }
+    },
+    .shunt_resistor = 0.04f
+};
+
+proxy::DipSwitch<4>::Config dip_switch_config = {
+    .gpio_array = {{
+        {
+            .port = GPIOC,
+            .pin = GPIO_PIN_7
+        },
+        {
+            .port = GPIOB,
+            .pin = GPIO_PIN_15
+        },
+        {
+            .port = GPIOB,
+            .pin = GPIO_PIN_14
+        },
+        {
+            .port = GPIOB,
+            .pin = GPIO_PIN_13
+        }
+    }}
 };
 
 #endif // __TARGET_HPP__
