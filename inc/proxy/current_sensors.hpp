973f2e6a981bbff70e8c942b3290edf0b45634cd
PedroDeSanti <phmartinsanti@gmail.com>
2024-03-20 21:49:16 -0300

    ♻️ Refactor current sensor classes
    Co-authored-by: Gabriel Cosme Barbosa <GabrielCosme@users.noreply.github.com>
    Co-authored-by: Matheus Rezende <Matheus3007@users.noreply.github.com>

---
+++ inc/proxy/current_sensors.hpp
@@ -0,0 +1,72 @@
+/**
+ * @file current_sensors.hpp
+ *
+ * @brief Proxy CurrentSensors class header
+ *
+ * @date 03/2024
+ */
+
+#ifndef __CURRENT_SENSORS_HPP__
+#define __CURRENT_SENSORS_HPP__
+
+#include <cstdint>
+
+#include "hal/adc_dma.hpp"
+
+namespace proxy {
+/**
+ * @brief Class for controlling CurrentSensors
+ */
+template <uint8_t num_of_sensors>
+class CurrentSensors {
+    public:
+        /**
+         * @brief configuration structure for current sensors
+         */
+        struct Config {
+            hal::AdcDma::Config adc;
+            float               shunt_resistor;
+        };
+
+        /**
+         * @brief Constructor for the CurrentSensors class
+         *
+         * @param current_sensors_config Configuration for the current sensors
+         */
+        CurrentSensors(const Config& current_sensors_config);
+
+        /**
+         * @brief Get the current from the sensor
+         *
+         * @param sensor_index Index of the sensor
+         * @return float Current reading from the sensor
+         */
+        float get_current(uint8_t sensor_index);
+
+        /**
+         * @brief Get the raw reading from the current sensor
+         *
+         * @param sensor_index Index of the sensor
+         * @return uint16_t Current reading from the sensor
+         */
+        uint32_t get_current_raw(uint8_t sensor_index);
+
+    private:
+        /**
+         * @brief ADC DMA handle
+         */
+        hal::AdcDma adc;
+
+        /**
+         * @brief Buffer to store the ADC values
+         */
+        volatile std::array<uint32_t, num_of_sensors> dma_buffer;
+
+        /**
+         * @brief Value of the shunt resistor in ohms
+         */
+        float shunt_resistor;
+};
+}  // namespace proxy
+
+#endif // __CURRENT_SENSORS_HPP__
---
+++ src/proxy/current_sensors.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file current_sensors.cpp
+ *
+ * @brief Proxy CurrentSensors class implementation
+ *
+ * @date 03/2024
+ */
+
+#include "proxy/current_sensors.hpp"
+
+namespace proxy {
+static constexpr float reference_voltage{3.3f};
+
+static constexpr uint32_t max_adc_reading{4095};
+
+template <uint8_t num_of_sensors>
+CurrentSensors<num_of_sensors>::CurrentSensors(const Config& current_sensors_config) :
+    adc(current_sensors_config.adc_config), shunt_resistor(current_sensors_config.shunt_resistor) {
+    this->adc.start_dma(this->adc_buffer.data(), num_of_sensors);
+    this->adc.start_dma(this->adc_buffer.data(), num_of_sensors);
+}
+
+template <uint8_t num_of_sensors>
+float CurrentSensors<num_of_sensors>::get_current(uint8_t sensor_index) {
+    return reference_voltage * this->adc_buffer.at(sensor_index) / (max_adc_reading * this->shunt_resistor);
+}
+
+template <uint8_t num_of_sensors>
+uint32_t CurrentSensors<num_of_sensors>::get_current_raw(uint8_t sensor_index) {
+    return this->adc_buffer.at(sensor_index);
+}
+}  // namespace proxy
