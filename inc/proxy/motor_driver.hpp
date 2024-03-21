5364b59ed612c272801dc0c06d069ee780c1c5b7
PedroDeSanti <phmartinsanti@gmail.com>
2024-03-21 01:45:01 -0300

    ✨ Add MotorDriver class and implementation
    Co-authored-by: Gabriel Cosme Barbosa <GabrielCosme@users.noreply.github.com>

---
+++ inc/proxy/motor_driver.hpp
@@ -0,0 +1,94 @@
+/**
+ * @file motor_driver.hpp
+ *
+ * @brief Proxy MotorDriver class declaration
+ *
+ * @date 03/2024
+ */
+
+#ifndef __MOTOR_DRIVER_HPP__
+#define __MOTOR_DRIVER_HPP__
+
+#include <cstdint>
+
+#include "hal/pwm.hpp"
+#include "hal/gpio.hpp"
+
+namespace proxy {
+/**
+ * @brief Class for controlling a motor driver
+ */
+class MotorDriver {
+    public:
+        /**
+         * @brief Configuration structure for the motor driver
+         */
+        struct Config {
+            hal::Pwm::Config  pwm;
+            hal::Gpio::Config direction_gpio;
+            hal::Gpio::Config enable_gpio;
+        };
+
+        /**
+         * @brief Enum for rotation direction
+         */
+        enum RotationDirection {
+            FORWARD,
+            BACKWARD
+        };
+
+        /**
+         * @brief Construct a new  Motor Driver object
+         *
+         * @param motor_driver_config
+         */
+        MotorDriver(const Config& motor_driver_config);
+
+        /**
+         * @brief Enable the motor driver
+         */
+        void enable();
+
+        /**
+         * @brief Disable the motor driver
+         */
+        void disable();
+
+        /**
+         * @brief Set the speed of the motors
+         *
+         * @param speed Speed percentage of the motor
+         */
+        void set_speed(float speed);
+
+        /**
+         * @brief Stop the motors
+         */
+        void stop();
+
+    private:
+        /**
+         * @brief Set the rotation direction of the motor
+         *
+         * @param direction Rotation direction
+         */
+        void set_direction(RotationDirection direction);
+
+        /**
+         * @brief PWM object for controlling the motor speed
+         */
+        hal::Pwm pwm;
+
+        /**
+         * @brief GPIO object for controlling the motor rotation direction
+         */
+        hal::Gpio direction_gpio;
+
+        /**
+         * @brief GPIO handle for the motor driver enable pin
+         */
+        hal::Gpio enable_gpio;
+};
+}  // namespace proxy
+
+#endif // __MOTOR_DRIVER_HPP__
---
+++ src/proxy/motor_driver.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file motor_driver.cpp
+ *
+ * @brief Proxy MotorDriver class source
+ *
+ * @date 03/2024
+ */
+
+#include "proxy/motor_driver.hpp"
+
+namespace proxy {
+MotorDriver::MotorDriver(const Config& motor_driver_config) :
+    pwm{motor_driver_config.pwm},
+    direction_gpio{motor_driver_config.direction_gpio},
+    enable_gpio{motor_driver_config.enable_gpio} {
+    this->enable();
+    this->stop();
+}
+
+void MotorDriver::enable() {
+    this->enable_gpio.write(true);
+}
+
+void MotorDriver::disable() {
+    this->enable_gpio.write(false);
+}
+
+void MotorDriver::set_speed(float speed) {
+    if (speed > 0.0f) {
+        this->pwm.set_duty_cycle(speed);
+        this->set_direction(RotationDirection::FORWARD);
+    } else if (speed < 0.0f) {
+        this->pwm.set_duty_cycle(-speed);
+        this->set_direction(RotationDirection::BACKWARD);
+    } else {
+        this->stop();
+    }
+}
+
+void MotorDriver::stop() {
+    this->set_speed(0.0f);
+}
+
+void MotorDriver::set_direction(RotationDirection direction) {
+    this->direction_gpio.write((bool) direction);
+}
+}  // namespace proxy
