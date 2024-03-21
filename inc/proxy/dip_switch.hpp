0cbf81a67456bb27c9dbc8ede08dbc48add00f09
PedroDeSanti <phmartinsanti@gmail.com>
2024-03-21 01:46:00 -0300

    ♻️ Refactor DipSwitch class to use const member functions
    Co-authored-by: Gabriel Cosme Barbosa <GabrielCosme@users.noreply.github.com>

--- inc/proxy/dip_switch.hpp
+++ inc/proxy/dip_switch.hpp
@@ -21,17 +21,40 @@ namespace proxy {
 template <uint8_t num_of_switches>
 class DipSwitch {
     public:
+        /**
+         * @brief Configuration struct for DipSwitch
+         */
         struct Config {
             std::array<hal::Gpio::Config, num_of_switches> gpio_array;
         };
 
+        /**
+         * @brief Construct a new Dip Switch object
+         *
+         * @param dip_switch_config Configuration struct for DipSwitch
+         */
         DipSwitch(const Config& dip_switch_config);
 
-        bool get_switch_state(uint8_t switch_index);
+        /**
+         * @brief Get the state of a switch
+         *
+         * @param switch_index Index of the switch
+         * @return true if the switch is on
+         * @return false if the switch is off
+         */
+        bool get_switch_state(uint8_t switch_index) const;
 
-        uint8_t get_switches_value();
+        /**
+         * @brief Get the value of all switches
+         *
+         * @return uint8_t Value of all switches
+         */
+        uint8_t get_switches_value() const;
 
     private:
+        /**
+         * @brief Array of GPIOs for the switches
+         */
         std::array<hal::Gpio, num_of_switches> gpio_array;
 };
 }  // namespace proxy
--- src/proxy/dip_switch.cpp
+++ src/proxy/dip_switch.cpp
@@ -13,17 +13,17 @@ namespace proxy {
 template <uint8_t num_of_sensors>
 DipSwitch<num_of_sensors>::DipSwitch(const Config& dip_switch_config) {
     for (uint8_t i = 0; i < num_of_sensors; i++) {
-        gpio_array[i] = dip_switch_config.gpio_array[i];
+        this->gpio_array[i] = dip_switch_config.gpio_array[i];
     }
 }
 
 template <uint8_t num_of_sensors>
-bool DipSwitch<num_of_sensors>::get_switch_state(uint8_t switch_index) {
+bool DipSwitch<num_of_sensors>::get_switch_state(uint8_t switch_index) const {
     return gpio_array.at(switch_index).read();
 }
 
 template <uint8_t num_of_sensors>
-uint8_t DipSwitch<num_of_sensors>::get_switches_value() {
+uint8_t DipSwitch<num_of_sensors>::get_switches_value() const {
     uint8_t switches_value = 0;
 
     for (uint8_t i = 0; i < num_of_sensors; i++) {
