375602ac8a923a887acd1d84f202e68ebe119680
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-03-22 14:17:13 -0300

    🧪 Add torque sensors proxy class

---
+++ inc/proxy/torque_sensors.hpp
@@ -0,0 +1,68 @@
+/**
+ * @file torque_sensors.hpp
+ *
+ * @brief Proxy TorqueSensors class header
+ *
+ * @date 03/2024
+ */
+
+#ifndef __TORQUE_SENSORS_HPP__
+#define __TORQUE_SENSORS_HPP__
+
+#include <cstdint>
+
+#include "proxy/current_sensors.hpp"
+
+namespace proxy {
+/**
+ * @brief Class for controlling TorqueSensors
+ */
+template <uint8_t num_of_sensors>
+class TorqueSensors {
+    public:
+        /**
+         * @brief configuration structure for torque sensors
+         */
+        struct Config {
+            CurrentSensors<num_of_sensors>::Config current_sensor;
+        };
+
+        /**
+         * @brief Constructor for the TorqueSensors class
+         *
+         * @param torque_sensors_config Configuration for the torque sensors
+         */
+        TorqueSensors(Config& torque_sensors_config);
+
+        /**
+         * @brief Get the torque from the sensor
+         *
+         * @param sensor_index Index of the sensor
+         * @return float Torque reading from the sensor in N*m
+         */
+        float get_torque(uint8_t sensor_index) const;
+
+        /**
+         * @brief Get the current from the sensor
+         *
+         * @param sensor_index Index of the sensor
+         * @return float Current reading from the sensor in amps
+         */
+        float get_current(uint8_t sensor_index) const;
+
+        /**
+         * @brief Maximum torque that can be measured by the sensor
+         */
+        static constexpr float max_torque = 50.0f;
+
+    private:
+        /**
+         * @brief Current sensors object
+         */
+        template CurrentSensors<num_of_sensors> current_sensors;
+};
+}  // namespace proxy
+
+#include "../src/proxy/torque_sensors.cpp"
+
+#endif // __TORQUE_SENSORS_HPP__
---
+++ src/proxy/torque_sensors.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file torque_sensors.cpp
+ *
+ * @brief Proxy TorqueSensors class implementation
+ *
+ * @date 03/2024
+ */
+
+#ifndef __TORQUE_SENSORS_CPP__
+#define __TORQUE_SENSORS_CPP__
+
+#include "proxy/torque_sensors.hpp"
+
+namespace proxy {
+template <uint8_t num_of_sensors>
+TorqueSensors<num_of_sensors>::TorqueSensors(Config& torque_sensors_config) :
+    current_sensors{torque_sensors_config.current_sensors} {
+}
+
+template <uint8_t num_of_sensors>
+float TorqueSensors<num_of_sensors>::get_torque(uint8_t sensor_index) const {
+    return this->current_sensors.get_current_raw(sensor_index) * TorqueSensor::max_torque / hal::AdcDma::max_reading;
+}
+
+template <uint8_t num_of_sensors>
+float TorqueSensors<num_of_sensors>::get_current(uint8_t sensor_index) const {
+    return this->current_sensors.get_current(sensor_index);
+}
+}  // namespace proxy
+
+#endif // __TORQUE_SENSORS_CPP__
