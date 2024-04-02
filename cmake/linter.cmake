6a75db74bfc71321669e722b4a1c30fb35ea596e
PedroDeSanti <phmartinsanti@gmail.com>
2024-04-02 17:06:02 -0300

    🔧 Add linter status message on cmake

--- cmake/linter.cmake
+++ cmake/linter.cmake
@@ -3,8 +3,13 @@ if(ENABLE_LINTER OR ENABLE_LINTER_FIX)
     add_compile_options(-fms-extensions)
 
     if(ENABLE_LINTER)
+        message(STATUS "Enabling clang-tidy")
         set(ENABLE_LINTER_FIX OFF)
+
     else(ENABLE_LINTER)
+        message(STATUS "Enabling clang-tidy with fix")
         set(CMAKE_CXX_CLANG_TIDY "${CMAKE_CXX_CLANG_TIDY};--fix")
+
     endif(ENABLE_LINTER)
+
 endif()
