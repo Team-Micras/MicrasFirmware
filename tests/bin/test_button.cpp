1c2e04c248aac008af0e102be3fa05e2734d2e25
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-04-05 18:13:23 -0300

    ✅ Add button test

---
+++ tests/bin/test_button.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file test_button.cpp
+ *
+ * @brief Test for the Button class
+ *
+ * @date 05/2024
+ */
+
+#include "proxy/button.hpp"
+#include "proxy/led.hpp"
+#include "target.hpp"
+#include "test_core.hpp"
+
+int main() {
+    test_core_init();
+    proxy::Button button{button_config};
+    proxy::Led    led{led_config};
+
+    while (true) {
+        if (button.is_pressed()) {
+            led.turn_on();
+        } else {
+            led.turn_off();
+        }
+    }
+
+    return 0;
+}
