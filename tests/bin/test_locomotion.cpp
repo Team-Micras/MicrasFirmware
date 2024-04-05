33d5b4d5d45a99aae1c5ef7c3ff268edc0a28dff
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-04-05 18:12:32 -0300

    ✅ Add locomotion test

---
+++ tests/bin/test_locomotion.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file test_locomotion.cpp
+ *
+ * @brief Test for the Locomotion class
+ *
+ * @date 05/2024
+ */
+
+#include "hal/timer.hpp"
+#include "proxy/locomotion.hpp"
+#include "target.hpp"
+#include "test_core.hpp"
+
+int main() {
+    test_core_init();
+    proxy::Locomotion locomotion{locomotion_config};
+
+    while (true) {
+        locomotion.set_wheel_speed(50.0F, 0.0F);
+        hal::Timer::sleep_ms(1000);
+        locomotion.set_wheel_speed(-50.0F, 0.0F);
+        hal::Timer::sleep_ms(1000);
+        locomotion.set_wheel_speed(0.0F, 50.0F);
+        hal::Timer::sleep_ms(1000);
+        locomotion.set_wheel_speed(0.0F, -50.0F);
+        hal::Timer::sleep_ms(1000);
+
+        locomotion.set_speed(50.0F, 0.0F);
+        hal::Timer::sleep_ms(1000);
+        locomotion.set_speed(-50.0F, 0.0F);
+        hal::Timer::sleep_ms(1000);
+        locomotion.set_speed(0.0F, 50.0F);
+        hal::Timer::sleep_ms(1000);
+        locomotion.set_speed(0.0F, -50.0F);
+    }
+
+    return 0;
+}
