cad5cfb84f833b39bdd829f9721a42a425433637
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-04-09 00:27:49 -0300

    🐛 Fix ARGB duty cycle

--- micras_proxy/include/micras/proxy/argb.hpp
+++ micras_proxy/include/micras/proxy/argb.hpp
@@ -86,8 +86,8 @@ private:
     static constexpr uint8_t bits_per_color{8};
     static constexpr uint8_t colors_per_led{3};
     static constexpr uint8_t bits_per_led{bits_per_color * colors_per_led};
-    static constexpr float   low_duty_cycle{0.32F};   // NOLINT(bugprone-dynamic-static-initializers)
-    static constexpr float   high_duty_cycle{0.64F};  // NOLINT(bugprone-dynamic-static-initializers)
+    static constexpr float   low_duty_cycle{32.0F};   // NOLINT(bugprone-dynamic-static-initializers)
+    static constexpr float   high_duty_cycle{64.0F};  // NOLINT(bugprone-dynamic-static-initializers)
     static constexpr uint8_t reset_length{40};
 
     /**
