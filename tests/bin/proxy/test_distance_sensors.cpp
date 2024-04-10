a336eaa82e753ac59da1b4c605dbdde937a705c1
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-04-10 01:29:44 -0300

    🥅 Fix array size in distance sensors test

--- tests/bin/proxy/test_distance_sensors.cpp
+++ tests/bin/proxy/test_distance_sensors.cpp
@@ -10,7 +10,7 @@
 
 using namespace micras;  // NOLINT(google-build-using-namespace)
 
-static volatile float distance[8];  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+static volatile float distance[4];  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
 int main(int argc, char* argv[]) {
     TestCore::init(argc, argv);
