bee129406c5329c29fe5b3bb8548065af9195974
PedroDeSanti <phmartinsanti@gmail.com>
2024-04-10 19:39:26 -0300

    ✅ Update test_button.cpp for using rgb_led

--- tests/bin/proxy/test_button.cpp
+++ tests/bin/proxy/test_button.cpp
@@ -12,14 +12,24 @@ using namespace micras;  // NOLINT(google-build-using-namespace)
 
 int main(int argc, char* argv[]) {
     TestCore::init(argc, argv);
-    proxy::Button button{button_config};
-    proxy::Led    led{led_config};
+    proxy::Button  button{button_config};
+    proxy::Led     led{led_config};
+    proxy::Argb<2> argb{argb_config};
 
-    TestCore::loop([&button, &led]() {
-        if (button.get_status() != proxy::Button::Status::NO_PRESS) {
-            led.toggle();
+    proxy::Argb<2>::Color color{0, 0, 0};
+
+    TestCore::loop([&button, &led, &argb, &color]() {
+        button.is_pressed() ? led.turn_on() : led.turn_off();
+        proxy::Button::Status status = button.get_status();
+
+        if (status == proxy::Button::Status::SHORT_PRESS) {
+            color = {255, 0, 0};
+        } else if (status == proxy::Button::Status::LONG_PRESS) {
+            color = {0, 255, 0};
+        } else if (status == proxy::Button::Status::EXTRA_LONG_PRESS) {
+            color = {0, 0, 255};
         }
-    });
 
-    return 0;
+        argb.set_color(color);
+    });
 }
