9045f2e22e777987e87eddbc3ce98f097d968975
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-04-22 22:31:31 -0300

    ✨ Add constants.hpp

---
+++ config/constants.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file constants.hpp
+ *
+ * @brief Robot specific constants
+ *
+ * @date 04/2024
+ */
+
+#ifndef MICRAS_CONSTANTS_HPP
+#define MICRAS_CONSTANTS_HPP
+
+namespace micras {
+constexpr float wheel_radius{0.0105};  // meters
+
+constexpr float wheel_separation{0.0585};  // meters
+
+}  // namespace micras
+
+#endif  // MICRAS_CONSTANTS_HPP
