eda1c16e0b73d8f76c6789f93152fca8c49e4e86
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-09-03 20:27:12 -0300

    ✨ Add read_register function to rotary_sensors

--- tests/src/proxy/test_rotary_sensors.cpp
+++ tests/src/proxy/test_rotary_sensors.cpp
@@ -10,17 +10,32 @@
 
 using namespace micras;  // NOLINT(google-build-using-namespace)
 
-static volatile float
-    test_left_position{};  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables, *-avoid-c-arrays)
-static volatile float
-    test_right_position{};  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables, *-avoid-c-arrays)
+static volatile float test_left_position{};   // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+static volatile float test_right_position{};  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+
+static volatile uint16_t test_resolution_byte{};
+static volatile uint16_t test_decimal_byte{};
 
 int main(int argc, char* argv[]) {
     TestCore::init(argc, argv);
-    proxy::RotarySensor rotary_sensor_left{rotary_sensor_left_config};
+    // proxy::RotarySensor rotary_sensor_left{rotary_sensor_left_config};
     proxy::RotarySensor rotary_sensor_right{rotary_sensor_right_config};
 
-    TestCore::loop([&rotary_sensor_left, &rotary_sensor_right]() {
+    proxy::RotarySensor::CommandFrame command_frame{};
+    proxy::RotarySensor::DataFrame    data_frame{};
+
+    command_frame.fields.address = proxy::RotarySensor::Registers::settings2_addr;
+    data_frame.fields.data = rotary_sensor_right_config.registers.settings2.raw;
+    rotary_sensor_right.write_register(command_frame, data_frame);
+
+    command_frame.fields.address = proxy::RotarySensor::Registers::settings3_addr;
+    data_frame.fields.data = rotary_sensor_right_config.registers.settings3.raw;
+    rotary_sensor_right.write_register(command_frame, data_frame);
+
+    test_decimal_byte = rotary_sensor_right.read_register(proxy::RotarySensor::Registers::settings2_addr) & (1 << 5);
+    test_resolution_byte = rotary_sensor_right.read_register(proxy::RotarySensor::Registers::settings3_addr) >> 5;
+
+    TestCore::loop([&rotary_sensor_right]() {
         test_left_position = rotary_sensor_left.get_position();
         test_right_position = rotary_sensor_right.get_position();
     });
