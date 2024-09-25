d16ae7f2c8ce1ea7dbcc248362b37bd7dcc556a3
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-09-24 21:14:05 -0300

    👷 Add msys to windows check on CMake

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -4,7 +4,7 @@
 # @see: https://stackoverflow.com/questions/11269833/cmake-selecting-a-generator-within-cmakelists-txt
 # 04/2023
 
-if(CMAKE_HOST_WIN32)
+if(CMAKE_HOST_WIN32 OR CMAKE_HOST_SYSTEM_NAME STREQUAL MSYS)
     set(CMAKE_GENERATOR "MSYS Makefiles" CACHE INTERNAL "" FORCE)
     message(STATUS "Windows Host - CMAKE_GENERATOR set to: ${CMAKE_GENERATOR}")
 endif()
