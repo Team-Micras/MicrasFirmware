2d7088aae6fc33fe043170f296df1e6fcfb35d53
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-12-29 05:21:23 -0300

    💩 Add code generation without login

--- docker/scripts/cube.sh
+++ docker/scripts/cube.sh
@@ -1,5 +1,4 @@
-# run fromm project root `bash docker/scripts/cube.sh`
 Xvfb :10 -ac > /dev/null &
 export DISPLAY=:10
-$CUBE_PATH/jre/bin/java -jar $CUBE_PATH/STM32CubeMX -q /MicrasFirmware/docker/scripts/.cube
+java -jar $CUBE_PATH/STM32CubeMX -q /MicrasFirmware/docker/scripts/.cube
 pkill -f Xvfb
