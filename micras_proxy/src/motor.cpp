eb96d8922e10d25a6609db5956a7a4e920f48155
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2025-04-14 17:18:05 -0300

    🔥 Remove deadzone from motor

--- micras_proxy/src/motor.cpp
+++ micras_proxy/src/motor.cpp
@@ -8,23 +8,13 @@
 #include "micras/proxy/motor.hpp"
 
 namespace micras::proxy {
-Motor::Motor(const Config& config) :
-    backwards_pwm{config.backwards_pwm},
-    forward_pwm{config.forward_pwm},
-    max_stopped_command{config.max_stopped_command},
-    deadzone{config.deadzone} {
+Motor::Motor(const Config& config) : backwards_pwm{config.backwards_pwm}, forward_pwm{config.forward_pwm} {
     this->set_command(0.0F);
 }
 
 void Motor::set_command(float command) {
     const bool is_positive = (command >= 0.0F);
     command = std::abs(command);
-
-    if (command <= max_stopped_command) {
-        command = 0.0F;
-    } else {
-        command = core::remap(command, 0.0F, 100.0F, this->deadzone, 100.0F);
-    }
 
     if (is_positive) {
         this->forward_pwm.set_duty_cycle(command);
