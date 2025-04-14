c0d5bc8e31a33f53536f88bd8a64a05efc633188
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2025-04-14 17:18:30 -0300

    ✨ Add Speed Controller

---
+++ micras_nav/include/micras/nav/speed_controller.hpp
@@ -0,0 +1,72 @@
+/**
+ * @file
+ */
+
+#ifndef MICRAS_NAV_SPEED_CONTROLLER_HPP
+#define MICRAS_NAV_SPEED_CONTROLLER_HPP
+
+#include "micras/core/pid_controller.hpp"
+#include "micras/nav/state.hpp"
+
+namespace micras::nav {
+/**
+ * @brief Class to calculate a command to go to a point.
+ */
+class SpeedController {
+public:
+    /**
+     * @brief Configuration struct for the SpeedController class.
+     */
+    struct Config {
+        struct FeedForward {
+            float bias;
+            float speed;
+            float acceleration;
+        };
+
+        core::PidController::Config linear_pid;
+        core::PidController::Config angular_pid;
+        FeedForward                 left_feed_forward;
+        FeedForward                 right_feed_forward;
+    };
+
+    /**
+     * @brief Construct a new SpeedController object.
+     *
+     * @param config The configuration for the SpeedController class.
+     */
+    SpeedController(const Config& config);
+
+    /**
+     * @brief Calculate the command to go to a point.
+     *
+     * @param state The current state of the robot.
+     * @param goal The goal point.
+     * @param elapsed_time The time elapsed since the last update.
+     * @return The command to go to the point.
+     */
+    Twist action(const Twist& current_twist, const Twist& desired_twist, float elapsed_time);
+
+    /**
+     * @brief Reset the PID controllers.
+     */
+    void reset();
+
+private:
+    /**
+     * @brief PID controller for stopping at the goal.
+     */
+    core::PidController linear_pid;
+
+    /**
+     * @brief PID controller for the orientation.
+     */
+    core::PidController angular_pid;
+
+    Config::FeedForward left_feed_forward;
+
+    Config::FeedForward right_feed_forward;
+};
+}  // namespace micras::nav
+
+#endif  // MICRAS_NAV_SPEED_CONTROLLER_HPP
---
+++ micras_nav/src/speed_controller.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file
+ */
+
+#include <cmath>
+
+#include "micras/core/utils.hpp"
+#include "micras/nav/speed_controller.hpp"
+
+namespace micras::nav {
+SpeedController::SpeedController(const Config& config) :
+    linear_pid(config.linear_pid),
+    angular_pid(config.angular_pid),
+    left_feed_forward{config.left_feed_forward},
+    right_feed_forward{config.right_feed_forward} { }
+
+Twist SpeedController::action(const Twist& current_twist, const Twist& desired_twist, float elapsed_time) {
+    this->linear_pid.set_setpoint(desired_twist.linear);
+    this->angular_pid.set_setpoint(desired_twist.angular);
+
+    float linear_command = this->linear_pid.update(current_twist.linear, elapsed_time);
+    float angular_command = this->angular_pid.update(current_twist.angular, elapsed_time);
+
+    float left_command = linear_command - angular_command;
+    float right_command = linear_command + angular_command;
+
+    float current_left_speed = current_twist.linear - current_twist.angular;
+    float desired_left_speed = desired_twist.linear - desired_twist.angular;
+    float left_ff = this->left_feed_forward.bias + this->left_feed_forward.speed * desired_left_speed +
+                    this->left_feed_forward.acceleration * (desired_left_speed - current_left_speed) / elapsed_time;
+
+    float current_right_speed = current_twist.linear + current_twist.angular;
+    float desired_right_speed = desired_twist.linear + desired_twist.angular;
+    float right_ff = this->right_feed_forward.bias + this->right_feed_forward.speed * desired_right_speed +
+                     this->right_feed_forward.acceleration * (desired_right_speed - current_right_speed) / elapsed_time;
+
+    return {left_command + left_ff, right_command + right_ff};
+}
+
+void SpeedController::reset() {
+    this->linear_pid.reset();
+    this->angular_pid.reset();
+}
+}  // namespace micras::nav
