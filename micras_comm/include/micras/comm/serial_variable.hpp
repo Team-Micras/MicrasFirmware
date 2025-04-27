0a794ad68c1a9563518c6743264067dd8a1b8b19
PedroDeSanti <phmartinsanti@gmail.com>
2025-04-28 00:03:55 +0200

    ✨ Add ISerialVariable interface

---
+++ micras_comm/include/micras/comm/serial_variable.hpp
@@ -0,0 +1,24 @@
+#ifndef MICRAS_COMM_SERIAL_VARIABLE_HPP
+#define MICRAS_COMM_SERIAL_VARIABLE_HPP
+
+//todo nao gostei do nome da classe
+
+#include <string>
+
+#include "micras/proxy/serializable.hpp"
+
+namespace micras::comm {
+
+class ISerialVariable : public proxy::ISerializable {
+public:
+    virtual ~ISerialVariable() = default;
+
+    virtual std::string get_name() const = 0;
+    virtual std::string get_type() const = 0;
+    virtual uint16_t get_size() const = 0;
+    virtual bool is_read_only() const = 0;
+};
+
+}
+
+#endif  // MICRAS_COMM_SERIAL_VARIABLE_HPP
