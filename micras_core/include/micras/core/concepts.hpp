6871d6fc6951bba90b67308820becd15bd369ebe
PedroDeSanti <phmartinsanti@gmail.com>
2025-04-28 00:27:05 +0200

    ✨ Add concepts for Fundamental and Serializable types

---
+++ micras_core/include/micras/core/concepts.hpp
@@ -0,0 +1,12 @@
+#include <type_traits>
+#include "serializable.hpp"
+
+namespace micras::core {
+
+template <typename T>
+concept Fundamental = std::is_fundamental_v<T>;
+
+template <typename T>
+concept Serializable = std::is_base_of<ISerializable, T>::value;
+
+}
