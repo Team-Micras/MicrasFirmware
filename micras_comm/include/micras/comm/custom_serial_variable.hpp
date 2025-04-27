a0065d2b36b7f6db1aadaf52cfb16b762922aa0c
PedroDeSanti <phmartinsanti@gmail.com>
2025-04-28 00:04:06 +0200

    ✨ Add CustomSerialVariable class

---
+++ micras_comm/include/micras/comm/custom_serial_variable.hpp
@@ -0,0 +1,39 @@
+#ifndef MICRAS_COMM_CUSTOM_SERIAL_VARIABLE_HPP
+#define MICRAS_COMM_CUSTOM_SERIAL_VARIABLE_HPP
+
+#include <type_traits>
+#include "serial_variable.hpp"
+
+namespace micras::comm {
+
+template <typename T>
+concept Serializable = std::is_base_of<ISerialVariable, T>::value;
+
+template <Serializable T>
+class CustomSerialVariable : public ISerialVariable {
+private:
+    proxy::ISerializable* serializable_ptr;
+    std::string name;
+    bool read_only;
+
+public:
+    CustomSerialVariable(const std::string& name, T* serializable, bool read_only)
+        : serializable_ptr(serializable), name(name), read_only(read_only) {}
+
+    std::string get_name() const override { return name; }
+    std::string get_type() const override { return type_name<T>(); }
+    uint16_t get_size() const override { return 0; }
+    bool is_read_only() const override { return read_only; }
+
+    std::vector<uint8_t> serialize() const override {
+        return serializable_ptr->serialize();
+    }
+
+    void deserialize(const uint8_t* serial_data, uint16_t size) override {
+        if (read_only) return;
+        serializable_ptr->deserialize(serial_data, size);
+    }
+};
+}
+
+#endif  // MICRAS_COMM_CUSTOM_SERIAL_VARIABLE_HPP
