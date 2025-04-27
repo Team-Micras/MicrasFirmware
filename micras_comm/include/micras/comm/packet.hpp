045cb299cb8504f095795f63eabb0dde91c539b9
PedroDeSanti <phmartinsanti@gmail.com>
2025-04-27 20:33:27 +0200

    ✨ Implement Packet class for communication packet handling

---
+++ micras_comm/include/micras/comm/packet.hpp
@@ -0,0 +1,61 @@
+// start destination source payload_size payload check_sum end
+
+#ifndef MICRAS_COMM_PACKET_HPP
+#define MICRAS_COMM_PACKET_HPP
+
+#include <cstdint>
+#include <vector>
+
+namespace micras::comm {
+/**
+ * @brief Class for controlling the communication packets.
+ *
+ * @note The packet structure is defined as follows:
+ * - Header byte: 0x42
+ * - Payload type: 1 byte
+ * - Payload size: 2 bytes
+ * - Payload: variable size
+ * - Checksum: 1 byte
+ * - Tail byte: 0x7F
+ */
+class Packet {
+public:
+    static constexpr uint8_t header_byte{0x42};
+    static constexpr uint8_t tail_byte{0x7F};
+    static constexpr uint8_t escape_byte{0x7D};
+    static constexpr uint8_t minimum_size{7};
+
+    enum class PayloadType : uint8_t {
+        ping = 0x00,
+        pong = 0x01,
+        serial_variable_map_request = 0x02,
+        serial_variable_map_response = 0x03,
+        serial_variable = 0x04,
+        debug_log = 0x05,
+        error = 0x06,
+    };
+
+    explicit Packet(PayloadType type, const std::vector<uint8_t>& payload);
+
+    explicit Packet(PayloadType type);
+
+    explicit Packet(const std::vector<uint8_t>& serialized_packet);
+
+    std::vector<uint8_t> serialize() const;
+
+    void deserialize(const uint8_t* buffer, uint16_t size);
+
+    bool is_valid(const std::vector<uint8_t>& serialized_packet);
+
+private:
+    PayloadType type;
+
+    std::vector<uint8_t> payload;
+
+    std::vector<uint8_t> escape_payload(const std::vector<uint8_t>& payload) const;
+
+    std::vector<uint8_t> unescape_payload(const std::vector<uint8_t>& escaped_payload) const;
+};
+}  // namespace micras::comm
+
+#endif  // MICRAS_COMM_PACKET_HPP
---
+++ micras_comm/src/packet.cpp
@@ -0,0 +1,93 @@
+#include "micras/comm/packet.hpp"
+
+namespace micras::comm {
+Packet::Packet(PayloadType type, const std::vector<uint8_t>& payload) : type{type}, payload{payload} { }
+
+Packet::Packet(PayloadType type) : Packet(type, {0}) { }
+
+Packet::Packet(const std::vector<uint8_t>& serialized_packet) {
+    if (not this->is_valid(serialized_packet)) {
+        this->type = PayloadType::error;
+        this->payload = {0};
+        return;
+    }
+
+    this->type = static_cast<PayloadType>(serialized_packet[1]);
+    uint16_t             payload_size = (serialized_packet[2] << 8) | serialized_packet[3];
+    std::vector<uint8_t> read_payload(serialized_packet.begin() + 4, serialized_packet.end() - 2);
+    this->payload = this->unescape_payload(read_payload);
+}
+
+std::vector<uint8_t> Packet::serialize() const {
+    std::vector<uint8_t> data;
+
+    data.emplace_back(header_byte);
+    data.emplace_back(this->type);
+
+    data.emplace_back(this->payload.size() >> 8);
+    data.emplace_back(this->payload.size() & 0xFF);
+
+    std::vector<uint8_t> escaped_payload = this->escape_payload(this->payload);
+    data.insert(data.end(), escaped_payload.begin(), escaped_payload.end());
+
+    uint8_t checksum = 0;
+    for (uint16_t i = 1; i < data.size(); ++i) {
+        checksum += data[i];
+    }
+    checksum %= 256;
+    data.emplace_back(checksum);
+
+    data.emplace_back(tail_byte);
+
+    return data;
+}
+
+std::vector<uint8_t> Packet::escape_payload(const std::vector<uint8_t>& payload) const {
+    std::vector<uint8_t> escaped;
+
+    for (auto byte : payload) {
+        if ((byte == header_byte) || (byte == tail_byte) || (byte == escape_byte)) {
+            escaped.emplace_back(escape_byte);
+        }
+        escaped.emplace_back(byte);
+    }
+
+    return escaped;
+}
+
+std::vector<uint8_t> Packet::unescape_payload(const std::vector<uint8_t>& escaped_payload) const {
+    std::vector<uint8_t> payload;
+
+    for (uint16_t i = 0; i < escaped_payload.size(); ++i) {
+        if (escaped_payload[i] == escape_byte) {
+            if (i + 1 < escaped_payload.size()) {
+                payload.emplace_back(escaped_payload[i + 1]);
+                ++i;
+            }
+        } else {
+            payload.emplace_back(escaped_payload[i]);
+        }
+    }
+
+    return payload;
+}
+
+bool Packet::is_valid(const std::vector<uint8_t>& serialized_packet) {
+    if (serialized_packet.size() < minimum_size) {
+        return false;
+    }
+
+    if (serialized_packet[0] != header_byte || serialized_packet.back() != tail_byte) {
+        return false;
+    }
+
+    uint8_t checksum = 0;
+    for (uint16_t i = 1; i < serialized_packet.size() - 2; ++i) {
+        checksum += serialized_packet[i];
+    }
+    checksum %= 256;
+
+    return checksum == serialized_packet[serialized_packet.size() - 2];
+}
+
+}  // namespace micras::comm
