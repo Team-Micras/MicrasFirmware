857b7b2d054a698d7172b9359c558ffc5908aaaf
PedroDeSanti <phmartinsanti@gmail.com>
2025-04-30 13:02:12 +0200

    ♻️  Refactor Bluetooth class to improve data handling and processing methods

--- micras_proxy/include/micras/proxy/bluetooth.hpp
+++ micras_proxy/include/micras/proxy/bluetooth.hpp
@@ -38,15 +38,37 @@ public:
     explicit Bluetooth(const Config& config);
 
     /**
-     * @brief Process all received messages and send chosen variables.
+     * @brief Process all received messages and send queued data.
+     *
+     * This method handles the actual sending and receiving of data over UART.
+     * It should be called regularly in the main loop.
      */
     void update();
 
+    /**
+     * @brief Queue data to be sent during the next update call.
+     *
+     * @param data Bytes to be sent over Bluetooth
+     */
     void send_data(std::vector<uint8_t> data);
 
+    /**
+     * @brief Get data that has been received since the last call.
+     *
+     * @return Vector containing received bytes (will be empty if no new data)
+     */
     std::vector<uint8_t> get_data();
 
 private:
+    /**
+     * @brief Process received data from UART
+     */
+    void process_rx_data();
+
+    /**
+     * @brief Send queued data to UART
+     */
+    void process_tx_data();
 
     /**
      * @brief Size of the UART buffers.
@@ -54,23 +76,32 @@ private:
     static constexpr uint16_t buffer_max_size{1000};
 
     /**
+     * @brief Maximum size of the tx queue.
+     */
+    static constexpr uint16_t tx_queue_max_size{buffer_max_size};
+
+    /**
      * @brief UART for the bluetooth connection.
      */
     hal::UartDma uart;
 
     /**
-     * @brief Buffers for the UART communication.
+     * @brief DMA buffers for the UART communication.
      */
+    ///@{
     std::array<uint8_t, buffer_max_size> rx_buffer{};
     std::array<uint8_t, buffer_max_size> tx_buffer{};
-
-    std::queue<uint8_t> tx_queue{};
+    ///@}
 
     /**
-     * @brief Cursor for navigating the buffers.
+     * @brief Buffer for data waiting to be sent in the next update
      */
-    uint16_t rx_cursor{};
-    uint16_t tx_cursor{};
+    std::vector<uint8_t> tx_queue{};
+
+    /**
+     * @brief Buffer for received data waiting to be retrieved
+     */
+    std::vector<uint8_t> received_data{};
 };
 }  // namespace micras::proxy
 
--- micras_proxy/src/bluetooth.cpp
+++ micras_proxy/src/bluetooth.cpp
@@ -3,55 +3,54 @@
  */
 
 #include <bit>
+#include <algorithm>
 
 #include "micras/proxy/bluetooth.hpp"
 
 namespace micras::proxy {
-Bluetooth::Bluetooth(const Config& config) : uart(config.uart) {
+Bluetooth::Bluetooth(const Config& config) : uart{config.uart} {
     this->uart.start_rx_dma(this->rx_buffer);
 }
 
 void Bluetooth::update() {
+    process_rx_data();
+    process_tx_data();
 }
 
-std::vector<uint8_t> Bluetooth::get_data() {
-    std::vector<uint8_t> data;
-
+void Bluetooth::process_rx_data() {
     this->uart.stop_rx_dma();
 
     uint16_t received_bytes = this->rx_buffer.size() - this->uart.get_rx_dma_counter() - 1;
 
     for (uint16_t i = 0; i < received_bytes; i++) {
-        data.emplace_back(this->rx_buffer.at(i));
+        this->received_data.emplace_back(this->rx_buffer.at(i));
     }
 
     this->uart.start_rx_dma(this->rx_buffer);
+}
 
+void Bluetooth::process_tx_data() {
+    if (this->tx_queue.empty() || this->uart.is_tx_busy()) {
+        return;
+    }
+
+    uint16_t bytes_to_send = std::min(this->tx_queue.size(), buffer_max_size);
+    for (uint16_t i = 0; i < bytes_to_send; i++) {
+        this->tx_buffer[i] = this->tx_queue[i];
+    }
+
+    this->tx_queue.erase(this->tx_queue.begin(), this->tx_queue.begin() + bytes_to_send);
+
+    this->uart.start_tx_dma({this->tx_buffer.data(), bytes_to_send});
+}
+
+std::vector<uint8_t> Bluetooth::get_data() {
+    std::vector<uint8_t> data = std::move(this->received_data);
+    this->received_data.clear();
     return data;
 }
 
 void Bluetooth::send_data(std::vector<uint8_t> data) {
-    if (this->uart.is_tx_busy()) {
-        for (const auto& byte : data) {
-            this->tx_queue.push(byte);
-        }
-
-        return;
-    }
-
-    this->tx_cursor = 0;
-
-    while (!this->tx_queue.empty()) {
-        this->tx_buffer.at(this->tx_cursor) = this->tx_queue.front();
-        this->tx_queue.pop();
-        this->tx_cursor += 1;
-    }
-
-    for (const auto& byte : data) {
-        this->tx_buffer.at(this->tx_cursor) = byte;
-        this->tx_cursor += 1;
-    }
-
-    this->uart.start_tx_dma({this->tx_buffer.data(), this->tx_cursor});
+    this->tx_queue.insert(this->tx_queue.end(), data.begin(), data.end());
 }
 }  // namespace micras::proxy
