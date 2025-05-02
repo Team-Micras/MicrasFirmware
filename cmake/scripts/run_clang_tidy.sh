e3cba7de0cda2fb73be1a9e3020667a594bfc1f4
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2025-05-01 22:08:28 -0300

    🐛 Fix run_clang_tidy script

--- cmake/scripts/run_clang_tidy.sh
+++ cmake/scripts/run_clang_tidy.sh
@@ -9,17 +9,14 @@ if [[ "$1" == "--fix" ]]; then
 fi
 
 run_clang_tidy() {
-    local output=$(clang-tidy $FIX_FLAG --quiet -p build $1 2>&1)
+    local output
+    output=$(clang-tidy $FIX_FLAG --quiet -p build $1 2>&1)
 
     if [[ $? -ne 0 ]]; then
-        echo "clang-tidy failed on $1" >&2
-        echo "$output" >&2
-        EXIT_CODE=1
+        echo "$output"
+        return 1
     fi
 }
-
-export FIX_FLAG EXIT_CODE
-export -f run_clang_tidy
 
 declare -a PIDS
 echo "Running clang-tidy on all source files..."
