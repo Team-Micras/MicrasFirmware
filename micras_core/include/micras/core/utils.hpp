9ed373539268b9037ef0e9c17eecaf201cac4fea
PedroDeSanti <phmartinsanti@gmail.com>
2025-05-20 00:17:27 +0200

    🐛 Fix type_name function prefix to include namespace in output

--- micras_core/include/micras/core/utils.hpp
+++ micras_core/include/micras/core/utils.hpp
@@ -153,15 +153,15 @@ constexpr auto type_name() {
     std::string_view suffix;
 #ifdef __clang__
     name = __PRETTY_FUNCTION__;
-    prefix = "auto type_name() [T = ";
+    prefix = "auto micras::core::type_name() [T = ";
     suffix = "]";
 #elif defined(__GNUC__)
     name = __PRETTY_FUNCTION__;
-    prefix = "constexpr auto type_name() [with T = ";
+    prefix = "constexpr auto micras::core::type_name() [with T = ";
     suffix = "]";
 #elif defined(_MSC_VER)
     name = __FUNCSIG__;
-    prefix = "auto __cdecl type_name<";
+    prefix = "auto __cdecl micras::core::type_name<";
     suffix = ">(void)";
 #endif
     name.remove_prefix(prefix.size());
