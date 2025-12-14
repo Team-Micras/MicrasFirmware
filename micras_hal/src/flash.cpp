f1c927d3dd8997b2abe3ae224d15bb44c4c75e8f
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2025-12-13 22:29:22 -0300

    🐛 Fix flash memory hal

--- micras_hal/src/flash.cpp
+++ micras_hal/src/flash.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <bit>
+#include <stm32h7xx_hal_flash.h>
 
 #include "micras/hal/flash.hpp"
 
@@ -15,60 +16,54 @@ namespace micras::hal {
 static const uint32_t base_address = FLASH_BASE + FLASH_SIZE - 8;
 
 // NOLINTNEXTLINE(*-avoid-c-arrays)
-void Flash::read(uint32_t address, uint64_t data[], uint32_t size) {
+void Flash::read(uint32_t address, uint32_t data[], uint32_t size) {
     const uint32_t end = base_address - 8 * address;
 
     for (address = end - 8 * (size - 1); address <= end; address += 8, data++) {
-        (*data) = *(std::bit_cast<uint64_t*>(address));
+        (*data) = *(std::bit_cast<uint32_t*>(address));
     }
 }
 
 // NOLINTNEXTLINE(*-avoid-c-arrays)
-void Flash::read(uint16_t page, uint16_t page_address, uint64_t data[], uint32_t size) {
-    read(page * double_words_per_page + page_address, data, size);
+void Flash::read(uint16_t page, uint16_t page_address, uint32_t data[], uint32_t size) {
+    read(page * flash_words_per_sector + page_address, data, size);
 }
 
 // NOLINTNEXTLINE(*-avoid-c-arrays)
-void Flash::write(uint32_t address, const uint64_t data[], uint32_t size) {
+void Flash::write(uint32_t address, const uint32_t data[], uint32_t size) {
     HAL_FLASH_Unlock();
 
-    // const uint32_t end = base_address - 8 * address;
-    // address = end - 8 * (size - 1);
+    const uint32_t end = base_address - 8 * address;
+    address = end - 8 * (size - 1 + (bytes_per_flashword - size % bytes_per_flashword));
 
-    // while (address <= end) {
-    //     if (size >= double_words_per_row) {
-    //         HAL_FLASH_Program(FLASH_TYPEPROGRAM_FAST, address, std::bit_cast<uint32_t>(data));
-    //         address += bytes_per_row;
-    //         data += double_words_per_row;
-    //         size -= double_words_per_row;
-    //     } else {
-    //         HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, address, *data);
-    //         address += 8;
-    //         data++;
-    //         size--;
-    //     }
-    // }
+    while (size > 0) {
+        HAL_FLASH_Program(FLASH_TYPEPROGRAM_FLASHWORD, address, std::bit_cast<uint32_t>(data));
+        size -= bytes_per_flashword;
+        address += bytes_per_flashword;
+        data += bytes_per_flashword;
+    }
 
     HAL_FLASH_Lock();
 }
 
 // NOLINTNEXTLINE(*-avoid-c-arrays)
-void Flash::write(uint16_t page, uint16_t page_address, const uint64_t data[], uint32_t size) {
-    write(page * double_words_per_page + page_address, data, size);
+void Flash::write(uint16_t page, uint16_t page_address, const uint32_t data[], uint32_t size) {
+    write(page * flash_words_per_sector + page_address, data, size);
 }
 
 void Flash::erase_pages(uint16_t page, uint16_t number_of_pages) {
-    // FLASH_EraseInitTypeDef erase_struct = {
-    //     .TypeErase = FLASH_TYPEERASE_PAGES,
-    //     .Banks = FLASH_BANK_2,
-    //     .Page = FLASH_PAGE_NB - page - number_of_pages,
-    //     .NbPages = number_of_pages,
-    // };
+    FLASH_EraseInitTypeDef erase_struct = {
+        .TypeErase = FLASH_TYPEERASE_SECTORS,
+        .Banks = FLASH_BANK_1,
+        .Sector = FLASH_SECTOR_TOTAL - page - number_of_pages,
+        .NbSectors = number_of_pages,
+        .VoltageRange = FLASH_VOLTAGE_RANGE_4
+    };
 
-    // uint32_t page_error{};
+    uint32_t sector_error{};
 
     HAL_FLASH_Unlock();
-    // HAL_FLASHEx_Erase(&erase_struct, &page_error);
+    HAL_FLASHEx_Erase(&erase_struct, &sector_error);
     HAL_FLASH_Lock();
 }
 }  // namespace micras::hal
